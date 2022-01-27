68fcb87ed85fd1e1be0320a07ad6768881bf5830
piorpick <dsppark472@gmail.com>
2022-01-27 18:13:32 +0900

    저장

---
+++ src/main/java/com/cos/blog/ProjectblogApplication.java
@@ -0,0 +1,13 @@
+package com.cos.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectblogApplication.class, args);
+	}
+
+}
